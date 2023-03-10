5a08744b958470f25e997b796cc2e124753094ae
Brian Yoder <brianyoder@gmail.com>
2023-03-10 08:53:50 -0800

    delete whitespace

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,8 +4,6 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
-    
-    
       public DbSet<Stylist> Stylists { get; set; }
       public DbSet<Client> Clients { get; set; }
       public HairSalonContext(DbContextOptions options) : base(options) { }
